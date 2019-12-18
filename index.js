9899d7ad6e1a94198895cf663aa50f99302080ad
rahil143 <pathanrahil143@gmail.com>
2019-12-18 11:06:01 +0530

    Successfully connected to ORacle DB

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+'use strict';
+
 const Joi = require('joi');
 const express = require('express');
+const oracledb = require('oracledb');
+const dbConfig = require('./dbconfig.js');
 const app = express();
 
 app.use(express.json());
-
-'use strict';
-
-const oracledb = require('oracledb');
-const dbConfig = require('./dbconfig.js');
 
 async function run() {
 
@@ -17,7 +16,12 @@ async function run() {
     // Get a non-pooled connection
     connection = await oracledb.getConnection(dbConfig);
 
-    console.log('Connection was successful!');
+	console.log('Connection was successful!');
+	
+	const result = await connection.execute(
+		`SELECT * FROM access_user` // bind value for :id
+	  );
+	  console.log(result.rows);
 
   } catch (err) {
     console.error(err);
